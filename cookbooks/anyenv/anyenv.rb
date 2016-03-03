a51150fba11eb3ddf3a079fb7272155e83961d78
yosuke0315 <wonderfulworld0315@gmail.com>
2016-03-03 13:16:12 +0900

    move anyenv

---
+++ cookbooks/anyenv/anyenv.rb
@@ -0,0 +1,39 @@
+# install anyenv
+HOME_DIR = "/home/#{node[:user]}"
+
+execute 'git clone https://github.com/riywo/anyenv ~/.anyenv' do
+  not_if "test -d #{HOME_DIR}/.anyenv"
+  user "#{node[:user]}"
+end
+
+directory "#{HOME_DIR}/.anyenv/plugins" do
+  not_if "test -d #{HOME_DIR}/.anyenv/plugins"
+  user "#{node[:user]}"
+end
+
+git "#{HOME_DIR}/.anyenv/plugins/anyenv-update" do
+  repository 'https://github.com/znz/anyenv-update.git'
+  user "#{node[:user]}"
+end
+
+git "#{HOME_DIR}/.anyenv/plugins/anyenv-git" do
+  repository 'https://github.com/znz/anyenv-git.git'
+  user "#{node[:user]}"
+end
+
+remote_file "#{HOME_DIR}/.bashrc" do
+  source './files/.bashrc'
+  mode '755'
+  owner "#{node[:user]}"
+  user "#{node[:user]}"
+end
+
+execute '/bin/bash -lc "anyenv install rbenv"' do
+  not_if "test -d #{HOME_DIR}/.anyenv/envs/rbenv"
+  user "#{node[:user]}"
+end
+
+execute '/bin/bash -lc "anyenv install ndenv"' do
+  not_if "test -d #{HOME_DIR}/.anyenv/envs/ndenv"
+  user "#{node[:user]}"
+end
