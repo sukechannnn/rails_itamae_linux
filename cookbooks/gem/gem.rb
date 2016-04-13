129e985218011382396b0738ce44d5beeb0426f7
yosuke0315 <wonderfulworld0315@gmail.com>
2016-04-13 17:24:40 +0900

    fix gem

--- cookbooks/gem/gem.rb
+++ cookbooks/gem/gem.rb
@@ -1,11 +1,11 @@
 node[:gems].each do |gem|
   execute "/bin/bash -lc \"gem install #{gem}\"" do
-    not_if "gem list | grep #{gem}"
+    not_if "/bin/bash -lc \"gem list | grep #{gem}\""
     user "#{node[:user]}"
   end
 end
 
 execute "/bin/bash -lc \"gem install rails -v #{node[:rails_version]}\"" do
-  not_if "rails -v | grep #{node[:rails_version]}"
+  not_if "/bin/bash -lc \"rails -v | grep #{node[:rails_version]}\""
   user "#{node[:user]}"
 end
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Itamae for Rails
 
-## versions
+## Installed version
 - ruby 2.3.0
 - rails 5.0.0.beta3
 - nginx 1.8.1
